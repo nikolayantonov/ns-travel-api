176f91c355ddfbf9eb87e5519e819cbca832ff63
Richard <richies75@gmail.com>
2019-10-09 10:49:38 +0200

    removed a parenthesis

--- src/main/java/com/nsapplication/api/travelapi/controller/TravelApiController.java
+++ src/main/java/com/nsapplication/api/travelapi/controller/TravelApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
+@RestController
 @RequestMapping(value = "/api/v1/")
 public class TravelApiController {
 
