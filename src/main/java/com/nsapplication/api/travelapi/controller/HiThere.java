e3d1053d21f65cd39ce744ac41a7ba920fac49e0
Richard <richies75@gmail.com>
2019-10-09 11:13:15 +0200

    Hello World working

--- src/main/java/com/nsapplication/api/travelapi/controller/HiThere.java
+++ src/main/java/com/nsapplication/api/travelapi/controller/HiThere.java
@@ -1,5 +1,10 @@
 package com.nsapplication.api.travelapi.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class HiThere {
     private String thisHere = "Hello World!";
 }
--- src/main/java/com/nsapplication/api/travelapi/controller/TripAdvisorController.java
+++ src/main/java/com/nsapplication/api/travelapi/controller/TripAdvisorController.java
@@ -13,16 +13,10 @@ public class TripAdvisorController {
 
     private static Logger log = LoggerFactory.getLogger(TripAdvisorController.class);
 
-    @GetMapping(value = "/thisplace")
-    public String returnWorld() {
-        log.info("Here");
-        return "Hello World";
-    }
-
     @GetMapping(value = "/here")
-    public ModelAndView helloWorld() {
-        log.info("This place");
-        return new ModelAndView("welcome", "message", new HiThere());
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World";
     }
 
 }
