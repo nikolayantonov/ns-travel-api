ea1095d8bb64040d8b830368bebf02da6ff49f51
Richard <richies75@gmail.com>
2019-10-09 11:06:59 +0200

    hate this

---
+++ src/main/java/com/nsapplication/api/travelapi/controller/HiThere.java
@@ -0,0 +1,5 @@
+package com.nsapplication.api.travelapi.controller;
+
+public class HiThere {
+    private String thisHere = "Hello World!";
+}
--- src/main/java/com/nsapplication/api/travelapi/controller/TripAdvisorController.java
+++ src/main/java/com/nsapplication/api/travelapi/controller/TripAdvisorController.java
@@ -1,42 +1,28 @@
 package com.nsapplication.api.travelapi.controller;
 
 
-import com.nsapplication.api.travelapi.NsRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriBuilder;
-import org.springframework.web.util.UriBuilderFactory;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.net.URI;
-
-@Controller (value = "/api/")
+@Controller
 public class TripAdvisorController {
 
     private static Logger log = LoggerFactory.getLogger(TripAdvisorController.class);
-//
-//    @PostMapping(name = "trip-request")
-//    public void getAllTripsPost(@RequestBody RequestInput ri) {
-//        LOGGER.info("In getAllTripsPost");
-//        LOGGER.info(ri.getDateTime());
-//    }
 
-//    @RequestMapping(name = "sometest", method = RequestMethod.POST, consumes = "application/json")
-//    public @ResponseBody RequestInput addInput(@RequestBody RequestInput ri) {
-//        log.info("Im here");
-//        String tac = NsRestClient.getURI(ri);
-//        URI uri = new UriBuilder()
-//                .path("www.something.com")
-//                .queryParam("one", "123", "two", "234", "three", "23423")
-//                .build();
+    @GetMapping(value = "/thisplace")
+    public String returnWorld() {
+        log.info("Here");
+        return "Hello World";
+    }
 
-
-
-
-//        return ri;
-//    }
+    @GetMapping(value = "/here")
+    public ModelAndView helloWorld() {
+        log.info("This place");
+        return new ModelAndView("welcome", "message", new HiThere());
+    }
 
 }
