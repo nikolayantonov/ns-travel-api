07d174454e33eabbc7a49bb1853d4800a0a82766
Richard <richies75@gmail.com>
2019-10-12 17:59:10 +0200

    yes please motherfucker! Fixed this stupid slf4j logging warning -- added exclusions

--- src/test/java/com/mobiquity/travelapi/integrations/nsclient/NsClientIntegrationTest.java
+++ src/test/java/com/mobiquity/travelapi/integrations/nsclient/NsClientIntegrationTest.java
@@ -1,7 +1,8 @@
 package com.mobiquity.travelapi.integrations.nsclient;
 
 import com.mobiquity.travelapi.rest.model.TravelRequest;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
