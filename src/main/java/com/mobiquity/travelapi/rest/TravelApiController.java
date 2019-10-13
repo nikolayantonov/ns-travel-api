0a5159546c926b63a7ea512c632e69b0f737dd19
Richard <richies75@gmail.com>
2019-10-13 20:07:24 +0200

    Testing - Context Loading
    Added some basic tests to ensure context loads.

--- src/main/java/com/mobiquity/travelapi/rest/TravelApiController.java
+++ src/main/java/com/mobiquity/travelapi/rest/TravelApiController.java
@@ -1,5 +1,9 @@
 package com.mobiquity.travelapi.rest;
 
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TravelApiController {
 
 }
--- src/test/java/com/mobiquity/travelapi/TravelApiApplicationTest.java
+++ src/test/java/com/mobiquity/travelapi/TravelApiApplicationTest.java
@@ -2,15 +2,22 @@ package com.mobiquity.travelapi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TravelApiApplicationTest {
 
+    @Autowired
+    TravelApiApplication travelApiApplication;
+
     @Test
     public void contextLoads() {
+        assertNotNull(travelApiApplication);
     }
 
 }
