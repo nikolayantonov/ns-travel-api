1f406458173dac8544e3e7ceddb36d796df88b5c
Richard <richies75@gmail.com>
2019-10-17 19:34:45 +0200

    TravelService - temp

--- src/main/java/com/mobiquity/travelapi/TravelService.java
+++ src/main/java/com/mobiquity/travelapi/TravelService.java
@@ -1,6 +1,7 @@
 package com.mobiquity.travelapi;
 
 import com.mobiquity.travelapi.integrations.nsclient.NsClient;
+import com.mobiquity.travelapi.integrations.nsclient.travelmodel.Route;
 import com.mobiquity.travelapi.integrations.nsclient.travelmodel.TravelPlan;
 import com.mobiquity.travelapi.rest.userresponsemodels.AllRoutesResponse;
 import com.mobiquity.travelapi.rest.userresponsemodels.MapTravelPlanToAllRoutesResponse;
@@ -10,6 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class TravelService {
 
@@ -17,6 +21,40 @@ public class TravelService {
     private NsClient nsClient;
 
     public TravelPlan getTravelPlanFromNs(TravelRequest travelRequest) {
-        return nsClient.getTravelPlan(travelRequest);
+//        return nsClient.getTravelPlan(travelRequest);
+
+
+        TravelPlan travelPlan = nsClient.getTravelPlan(travelRequest);
+        Map<String, String> locationAndTime = getLocationAndTime(travelPlan);
+
+    }
+
+    private HashMap<String, String> getLocationAndTime(TravelPlan travelPlan) {
+        Map<Route, Map<String, String>> routeDestinationAndTime = new HashMap<>();
+
+//        {Route 1 :    origin      - weather
+//                      destination - weather
+//         Route 2 :    origin      - weather
+//                      desination  - weather
+//
+//        }
+        Route firstRoute = travelPlan.getRoutes().get(0);
+
+        Map<Route, Map<String, String>> oneMap = new HashMap<>();
+
+        travelPlan.getRoutes().forEach(
+                route -> routeDestinationAndTime.put(route, Map.of(route.getOrigin().getStationName(), route.getOrigin().getPlannedDepartureTime(),
+                        route.getDestination().getStationName(), route.getDestination().getPlannedArrivalTime()))
+        );
+
+//        travelPlan.getRoutes().forEach(
+//                route -> maps.putAll(Map.of(route.getOrigin().getStationName(), route.getOrigin().getPlannedDepartureTime(),
+//                        route.getDestination().getStationName(), route.getDestination().getPlannedArrivalTime()))
+//        );
+
+
+
+
+
     }
 }
